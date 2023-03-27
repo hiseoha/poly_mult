b92dcc1f8069c9f79913ffe38d5a37470193d259
seoha <4540cm12@naver.com>
2023-03-27 20:47:27 +0900

    초기 커밋.

---
+++ poly_mutl/poly_mult2.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#define MAX_DEGREE 101
+
+typedef struct {
+	int degree;
+	float coef[MAX_DEGREE];
+} polynomial;
+
+polynomial poly_mult(polynomial A, polynomial B);
+
+polynomial poly_mult(polynomial A, polynomial B) {
+	polynomial C;
+	int Apos = 0, Bpos = 0, Cpos = 0;
+	int degree_a = A.degree;
+	int degree_b = B.degree;
+
+
+	int i, j;
+
+	for (i = 0; i < A.degree + B.degree + 1; i++) C.coef[i] = 0;
+	C.degree = A.degree + B.degree;
+
+	for (i = 0; i < A.degree + 1; i++)
+		for (j = 0; j < B.degree + 1; j++)
+			C.coef[i + j] += A.coef[i] * B.coef[j];
+
+	return C;
+}
+
+void print_poly(polynomial p) {
+	for (int i = p.degree; i > 0; i--)
+		printf("%3.1fx^%d + ", p.coef[p.degree - i], i);
+	printf("%3.1f \n", p.coef[p.degree]);
+}
+
+int main(void)
+{
+	polynomial a = { 6, {1, 9, 5, 0, 0, 7} };
+	polynomial b = { 3, {10, 1, 2, 5}};
+	polynomial c;
+
+	print_poly(a);
+	print_poly(b);
+	c = poly_mult(a, b);
+	printf("----\n°á°ú: ");
+	print_poly(c);
+
+	return 0;
+}
